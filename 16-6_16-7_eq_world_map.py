05094ad2266c4ccfa333ce84c8bc85b17f31286e
Dennis366 <dennisfellgard@outlook.com>
2026-02-06 09:25:10 +0100

    Add files via upload

---
+++ 16-6_16-7_eq_world_map.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import json
+import plotly.express as px
+
+
+path = Path(__file__).parent / 'eq_1_day_m1.geojson'
+contents = path.read_text(encoding='utf-8')
+all_eq_data = json.loads(contents)
+
+
+all_eq_dicts = all_eq_data['features']
+
+
+mags, lons, lats, titles = [], [], [], []
+
+
+for eq_dict in all_eq_dicts:
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    titles.append(eq_dict['properties']['title'])
+
+
+title = all_eq_data['metadata']['title']
+fig = px.scatter_geo(
+    lat=lats,
+    lon=lons,
+    size=mags,
+    title=title,
+    hover_name=titles,
+    color=mags,
+    color_continuous_scale='Viridis',
+    labels={'color':'Magnitude'},
+    projection='natural earth',
+)
+
+fig.show()
+
+
